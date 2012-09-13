8c444375abe91166ee836d3c0eb53dc3f285ed49
HackFisher <wxiaoyin@gmail.com>
2012-09-13 01:39:15 -0700

    modify dir

--- g.sh
+++ g.sh
@@ -1,7 +1,7 @@
 files=$(ls ./thrift | grep '.thrift$')
 for file in $files
 do
-    thrift --gen py  -out utils/. thrift/$file
-    thrift --gen cocoa -out cocoa/. thrift/$file
+    thrift --gen py  -out gen-py/. thrift/$file
+    thrift --gen cocoa -out gen-cocoa/. thrift/$file
     echo "$file"
 done
