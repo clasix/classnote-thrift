560dd044f7280f1e9fbfe43c67a45c7914219d23
HackFisher <wxiaoyin@gmail.com>
2012-10-30 04:59:11 -0400

    x

--- server/fcgi.py
+++ server/fcgi.py
@@ -4,4 +4,4 @@
 from app2 import app
 from flup.server.fcgi import WSGIServer
 
-WSGIServer(app, bindAddress='/tmp/flask_clasik_tk_app.sock').run()
+WSGIServer(app, bindAddress='/tmp/flask_clasik_tx_app.sock').run()
