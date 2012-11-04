2727a23c51a9165b56f1b57121b51afcfb265ae4
HackFisher <wxiaoyin@gmail.com>
2012-11-04 23:15:22 +0800

    rm test.py

--- server/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-#config = {'dev_mode': True, 'sqlite_path': 'init.db'}
-config = {'dev_mode': False, 'mysql_user': 'clasix', 'mysql_passwd':'clasix', 'host': 'clasix.tk'}
-import model
-db = model.db_factory(config)
-from ctrl import Ctrl
-ctrl = Ctrl(db)
